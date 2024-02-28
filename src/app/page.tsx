c1ce9e5145b79d5178422fff921c70249a20d71f
Naresh Bhatia <me@nareshbhatia.dev>
2024-02-28 17:13:25 -0500

    add PageHeader to HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
+import { Icons } from '@/components/icons';
+import {
+  PageActions,
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from '@/components/page-header';
+import { buttonVariants } from '@/components/ui/button';
+import { siteConfig } from '@/config/site';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+
 export default function HomePage() {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-semibold leading-none tracking-tight">
-        Shadcn Demo
-      </h1>
+    <div className="container relative">
+      <PageHeader>
+        <PageHeaderHeading>Shadcn Components Demo</PageHeaderHeading>
+        <PageHeaderDescription>
+          Beautifully designed components that you can copy and paste into your
+          apps. Accessible. Customizable. Open Source.
+        </PageHeaderDescription>
+        <PageActions>
+          <Link className={cn(buttonVariants())} href="/docs">
+            Get Started
+          </Link>
+          <Link
+            className={cn(buttonVariants({ variant: 'outline' }))}
+            href={siteConfig.links.github}
+            rel="noreferrer"
+            target="_blank"
+          >
+            <Icons.gitHub className="mr-2 size-4" />
+            GitHub
+          </Link>
+        </PageActions>
+      </PageHeader>
     </div>
   );
 }

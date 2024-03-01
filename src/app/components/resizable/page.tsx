2beff64f9995c0fd5361569203c04c71df4a7121
Naresh Bhatia <me@nareshbhatia.dev>
2024-03-01 17:01:45 -0500

    add Resizable component

--- src/app/components/resizable/page.tsx
+++ src/app/components/resizable/page.tsx
@@ -1,5 +1,11 @@
 import { DocPath } from '@/components/doc-path';
 import { Heading1 } from '@/components/heading1';
+import { Heading2 } from '@/components/heading2';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable';
 import { findSidebarNavItem } from '@/config/sidebar-nav';
 
 export default function ResizableDocs() {
@@ -11,6 +17,26 @@ export default function ResizableDocs() {
     <>
       <DocPath docTitle={title} />
       <Heading1 description={description} title={title} />
+
+      <Heading2>Example</Heading2>
+      <div className="h-72 p-10">
+        <ResizablePanelGroup
+          className="rounded-lg border"
+          direction="horizontal"
+        >
+          <ResizablePanel defaultSize={25}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Sidebar</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={75}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Content</span>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
     </>
   );
 }
